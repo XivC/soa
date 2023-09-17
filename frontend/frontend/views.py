5e76026013cfdbc6206de58fc8b4df0ae1ae9e33
Artyem Lavrov <artyemlavrov39@gmail.com>
2023-09-18 01:06:06 +0300

    Django proxy

--- frontend/frontend/views.py
+++ frontend/frontend/views.py
@@ -5,4 +5,37 @@ def index_view(request):
     return render(request, 'index.html')
 
 
+def redirect_to_crud(request):
+    target_url = 'http://example.com'
 
+    # Redirect all incoming requests to the target server
+    return HttpResponseRedirect(target_url)
+    target_url = 'http://example.com'
+
+    # Redirect all incoming requests to the target server
+    return HttpResponseRedirect(target_url)
+
+
+def proxy_api_request(request, *args, **kwargs):
+    # Define the URL of the target server
+    target_server_url = "http://api.example.com/api-endpoint"  # Replace with your API endpoint
+
+    try:
+        # Forward the client's request to the target server
+        target_url = f"{target_server_url}{request.get_full_path()}"
+        headers = dict(request.headers)
+        response = requests.request(request.method, target_url, params=request.GET, data=request.body, headers=headers)
+
+        # Return the target server's response to the client, including headers
+        django_response = HttpResponse(response.content, status=response.status_code,
+                                       content_type=response.headers['Content-Type'])
+
+        # Copy headers from the target server's response to the Django response
+        for key, value in response.headers.items():
+            django_response[key] = value
+
+        return django_response
+
+    except Exception as e:
+        # Handle any errors that occur during the request
+        return JsonResponse({'error': str(e)}, status=500)
--- frontend/static/view/pages/page_object.js
+++ frontend/static/view/pages/page_object.js
@@ -1,5 +1,4 @@
 import {Page} from "./page.js";
-import {DragonRepository} from "../../data/repository/dragon_repository.js";
 
 
 export class PageObject extends Page {
@@ -49,24 +48,4 @@ export class PageObject extends Page {
     onCreateEntity(fields, callback) {}
 
     onUpdateEntity(fields, callback) {}
-}
-
-export class PageObjectDragon extends PageObject {
-
-    constructor(props, entity) {
-        super(props, ['name', 'x', 'y', 'age', 'color', 'character', 'type'], entity);
-        this.dragonRepository = new DragonRepository()
-    }
-
-    onCreate() {
-        super.onCreate();
-    }
-
-    onUpdateEntity(fields, callback) {
-        this.dragonRepository.updateDragon(this.entity.id, fields, callback)
-    }
-
-    onCreateEntity(fields, callback) {
-        this.dragonRepository.createDragon(fields, callback)
-    }
 }
