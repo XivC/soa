2c9b8afd14672cf07753e8991cf8ffa45f6ea868
XivC <artemd55131@gmail.com>
2023-09-17 22:21:21 +0300

    Migreate to spring

---
+++ rest/src/test/java/ru/ifmo/soa/RestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ifmo.soa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
