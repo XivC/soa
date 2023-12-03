116c18a09345c09fb93862999b67a3adf3343116
XivC <artemd55131@gmail.com>
2023-12-03 16:23:30 +0300

    qwdqwdwdqwdqwdqwdqwdqwdqwd

--- rest/src/main/java/ru/ifmo/soa/dragons/api/schema/DragonResponse.java
+++ rest/src/main/java/ru/ifmo/soa/dragons/api/schema/DragonResponse.java
@@ -9,6 +9,7 @@
 package ru.ifmo.soa.dragons.api.schema;
 
 import lombok.*;
+import ru.ifmo.soa.persons.api.schema.PersonResponse;
 
 import javax.xml.bind.annotation.*;
 import javax.xml.datatype.XMLGregorianCalendar;
@@ -49,6 +50,8 @@ public class DragonResponse {
     protected String type;
     @XmlElement(required = true)
     protected String character;
+    @XmlElement
+    protected PersonResponse killer;
 
 
 }
